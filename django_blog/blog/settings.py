39cf71eb9dad855c5b6556319a6d78033dc79f8a
Fiker Robel <fikerrobel889@gmail.com>
2025-12-07 20:10:41 +0300

    Create settings.py

---
+++ django_blog/blog/settings.py
@@ -0,0 +1,10 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'blog',         # <-- MUST BE HERE
+    'taggit',       # <-- if you use django-taggit
+]
